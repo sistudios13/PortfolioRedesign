d5af545ca7c071a44b93f2779e31395e5a4c0a11
Westr <si.studios13@gmail.com>
2024-10-16 20:23:47 -0400

    Large Progress on home page

--- tailwind.colors.js
+++ tailwind.colors.js
@@ -4,7 +4,7 @@ tailwind.config = {
         colors: {
           lb: '#38b6ff', // Light Blue
           db: '#5271ff', // Dark Blue
-          lg: '#f0f4f8', // Dark Gray
+          lg: '#f0f4f8', // Light Gray
           dg: '#2a2a2a', // Dark Gray
           p: '#836fff'   // Purple
         }
